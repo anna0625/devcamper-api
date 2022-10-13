c88a7cc5b960fa42535129a9b4c3a408fd06a1b8
anna0625 <anna860625@gmail.com>
2022-10-13 11:22:47 +1100

    create/routes-and-responses-in-express

--- server.js
+++ server.js
@@ -6,6 +6,28 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
+app.get("/api/v1/bootcamps", (req, res) => {
+  res.status(200).json({ success: true, msg: "Show all bootcamps" });
+});
+
+app.get("/api/v1/bootcamps/:id", (req, res) => {
+  res.status(200).json({ success: true, msg: "Show bootcamp" });
+});
+
+app.post("/api/v1/bootcamps", (req, res) => {
+  res.status(200).json({ success: true, msg: "Create new bootcamp" });
+});
+
+app.put("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Display bootcamps ${req.params.id}` });
+});
+
+app.delete("/api/v1/bootcamps/:id", (req, res) => {
+  res.status(200).json({ success: true, msg: "Delete bootcamp" });
+});
+
 const PORT = process.env.PORT;
 
 app.listen(
